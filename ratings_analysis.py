dc087c9398fdc14c6afdf54772d9e02eba078db7
hafezgh <ghaemi.hafez@gmail.com>
2021-07-09 01:13:07 +0200

    plot emotion distribution

--- ratings_analysis.py
+++ ratings_analysis.py
@@ -1,5 +1,22 @@
 import csv
 import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+from scipy.stats import norm
 
-ratings = pd.read_csv('ratings_60.txt', delimiter=' ', names = ['id', 'HAP', 'SAD', 'SUR', 'ANG', 'DIS', 'PIC', 'sample'])
-print(ratings.head())
+
+ratings60 = pd.read_csv('ratings_60.txt', delimiter=' ', names = ['id', 'HAP', 'SAD', 'SUR', 'ANG', 'DIS', 'FEA', 'sample'])
+print(ratings60.head())
+hap = ratings60['HAP']
+sad = ratings60['SAD']
+sur = ratings60['SUR']
+ang = ratings60['ANG']
+dis = ratings60['DIS']
+fea = ratings60['FEA']
+
+
+plt.figure(figsize=(10,10))
+emotion = fea
+sns.distplot(emotion, fit=norm)
+plt.title("Emotion Distribution Plot", size=15, weight='bold')
+plt.show()
